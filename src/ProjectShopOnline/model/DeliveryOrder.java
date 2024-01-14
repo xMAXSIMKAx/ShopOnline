d52c8660fa4313201a09fe7361ccd7e7ff9c7f24
xMAXSIMKAx <maxymram@gmail.com>
2024-01-14 13:44:57 +0100

    change DeliveryOrder and add ConsoleShopView

--- src/ProjectShopOnline/model/DeliveryOrder.java
+++ src/ProjectShopOnline/model/DeliveryOrder.java
@@ -3,18 +3,18 @@ package ProjectShopOnline.model;
 public class DeliveryOrder implements Order {
     private Product product;
     private int quantity;
-    private double deliveryCostPercentage;
+
 
     public DeliveryOrder(Product product, int quantity, double deliveryCostPercentage) {
         this.product = product;
         this.quantity = quantity;
-        this.deliveryCostPercentage = deliveryCostPercentage;
+
     }
 
     @Override
     public double calculateTotalCost() {
         double productCost = product.getPrice() * quantity;
-        double deliveryCost = productCost * (deliveryCostPercentage / 100);
-        return productCost * deliveryCost;
+        double deliveryCost = productCost * 0.1;
+        return productCost + deliveryCost;
     }
 }
